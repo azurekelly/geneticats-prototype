ea2c988f8b0a4ed0510db493c146c47249d9b350
Azure Kelly <azkbaby@gmail.com>
2020-06-04 07:18:01 -0700

    Separate phenotype logic into conversion functions

--- src/js/Cat.js
+++ src/js/Cat.js
@@ -1,35 +1,28 @@
-import {countSetBits, fill} from './utils';
-import {palette} from './palette';
+import {fill} from './utils';
+import {genotypeToPhenotype, phenotypeToColors} from './genetics';
 
 function Cat(genotype, spritesheet, canvas) {
     this.genotype = genotype;
     this.spriteSheet = spritesheet;
     this.canvas = canvas;
-    this.dilute = countSetBits(genotype & 3);
-    this.tabby = countSetBits((genotype >> 2) & 3);
-    this.red = countSetBits((genotype >> 4) & 3);
-    this.white = countSetBits((genotype >> 6) & 3);
-    this.point = countSetBits((genotype >> 8) & 3);
-    this.muzzleLength = countSetBits((genotype >> 10) & 0xF);
-    this.eyeShape = countSetBits((genotype >> 14) & 0xF);
-    this.headShape = countSetBits((genotype >> 18) & 0xF);
-    this.earAngle = countSetBits((genotype >> 22) & 0xF);
+    this.phenotype = genotypeToPhenotype(genotype);
+    this.colors = phenotypeToColors(this.phenotype);
     this.constructHead();
 
     return this;
 }
 
 Cat.prototype.selectElements = function() {
-    this.earEl = this.spriteSheet.select('.ears' + (this.earAngle + 1)).clone();
-    this.headEl = this.spriteSheet.select('.head' + (this.headShape + 1)).clone();
-    this.eyeEl = this.spriteSheet.select('.eyes' + (this.eyeShape + 1)).clone();
-    this.lineEl = this.spriteSheet.select('.lines' + (this.headShape + 1)).clone();
-    this.muzzleEl = this.spriteSheet.select('.muzzle' + (this.muzzleLength + 1)).clone();
+    this.earEl = this.spriteSheet.select('.ears' + (this.phenotype.earSet + 1)).clone();
+    this.headEl = this.spriteSheet.select('.head' + (this.phenotype.headSize + 1)).clone();
+    this.eyeEl = this.spriteSheet.select('.eyes' + (this.phenotype.eyeShape + 1)).clone();
+    this.lineEl = this.spriteSheet.select('.lines' + (this.phenotype.headSize + 1)).clone();
+    this.muzzleEl = this.spriteSheet.select('.muzzle' + (this.phenotype.muzzleLength + 1)).clone();
     this.pointEl = this.spriteSheet.select('.point').clone();
     this.tabbyEl = this.spriteSheet.select('.tabby').clone();
     this.tortieEl = this.spriteSheet.select('.tortie').clone();
     this.torbieEl = this.spriteSheet.select('.torbie').clone();
-    this.whiteEl = this.spriteSheet.select((this.white === 2) ? '.highwhite' : '.lowwhite').clone();
+    this.whiteEl = this.spriteSheet.select((this.phenotype.white == 'high') ? '.highwhite' : '.lowwhite').clone();
     this.markingEl = this.canvas.g();
     this.whiteMarkingEl = this.canvas.g();
     this.headMask = this.canvas.g();
@@ -37,23 +30,23 @@ Cat.prototype.selectElements = function() {
     this.tortieMask = this.canvas.g(this.tortieEl.clone().attr({'fill': '#fff'}));
     this.eyeMask = this.canvas.g(this.eyeEl.select('.iris').clone().attr({'fill': '#fff'}));
 
-    if(this.point === 2) {
+    if(this.phenotype.point === 'point') {
         this.headMask.add(this.pointEl.clone().attr({'fill': '#fff'}));
         this.markingEl.add(this.pointEl);
     }
     else {
         this.headMask.add(this.lineEl.clone().attr({'fill': '#fff'}));
     }
-    if(this.tabby > 0) {
+    if(this.phenotype.tabby === 'tabby') {
         this.markingEl.add(this.tabbyEl);
     }
-    if(this.red === 1) {
+    if(this.phenotype.red === 'tortie') {
         this.markingEl.add(this.tortieEl);
-        if(this.tabby > 0) {
+        if(this.phenotype.tabby === 'tabby') {
             this.markingEl.add(this.torbieEl);
         }
     }
-    if(this.white > 0) {
+    if(this.phenotype.white !== 'non-white') {
         this.whiteMarkingEl.add(this.whiteEl);
     }
 
@@ -61,68 +54,17 @@ Cat.prototype.selectElements = function() {
 };
 
 Cat.prototype.colorElements = function() {
-    //must be called first for the colors to apply
-    this.parseColors();
-
-    fill(this.earEl.select('.left'), palette[this.baseColor]);
-    fill(this.earEl.select('.right'), palette[this.rightEarColor]);
-    fill(this.headEl, palette[this.headColor]);
-    fill(this.pointEl, palette[this.baseColor]);
-    fill(this.tabbyEl, palette[this.stripeColor]);
-    fill(this.tortieEl, palette[this.redColor]);
-    fill(this.torbieEl, palette[this.torbieColor]);
-    fill(this.whiteEl, palette['white']);
-    fill(this.eyeEl.select('.iris'), palette[this.eyeColor]);
-    fill(this.muzzleEl.select('.lips'), palette[this.muzzleColor]);
-    fill(this.muzzleEl.select('.jaw'), palette[this.jawColor]);
-};
-
-Cat.prototype.parseColors = function() {
-    this.baseColor = 'black';
-
-    if(this.red === 2) {
-        this.baseColor = 'red';
-    }
-    if(this.dilute === 2) {
-        this.baseColor += 'dilute';
-    }
-    if(this.tabby > 0) {
-        this.baseColor += 'tabby';
-    }
-    if(this.point === 2) {
-        this.baseColor += 'point';
-    }
-
-    this.stripeColor = this.baseColor.replace('tabby', '');
-    this.redColor = this.baseColor.replace('black', 'red');
-    this.torbieColor = this.redColor.replace('tabby', '');
-    this.muzzleColor = this.baseColor;
-    this.jawColor = this.baseColor;
-    this.rightEarColor = this.baseColor;
-
-    if(this.red === 1) {
-        this.jawColor = this.redColor;
-        this.rightEarColor = this.redColor;
-    }
-
-    if(this.point === 2) {
-        this.headColor = 'point';
-    }
-    else {
-        this.headColor = this.baseColor;
-    }
-
-    if(this.white > 0) {
-        this.muzzleColor = 'white';
-        this.jawColor = 'white';
-    }
-
-    if((this.point === 2) || (this.white === 2)) {
-        this.eyeColor = 'blue';
-    }
-    else {
-        this.eyeColor = 'yellow';
-    }
+    fill(this.earEl.select('.left'), this.colors.leftEar);
+    fill(this.earEl.select('.right'), this.colors.rightEar);
+    fill(this.headEl, this.colors.head);
+    fill(this.pointEl, this.colors.point);
+    fill(this.tabbyEl, this.colors.stripes);
+    fill(this.tortieEl, this.colors.tortie);
+    fill(this.torbieEl, this.colors.tortieStripes);
+    fill(this.whiteEl, this.colors.white);
+    fill(this.eyeEl.select('.iris'), this.colors.eyes);
+    fill(this.muzzleEl.select('.lips'), this.colors.muzzle);
+    fill(this.muzzleEl.select('.jaw'), this.colors.jaw);
 };
 
 Cat.prototype.maskElements = function() {
---
+++ src/js/genetics.js
@@ -0,0 +1,62 @@
+import {palette} from './palette';
+import {countSetBits} from './utils';
+
+export function genotypeToPhenotype(genotype) {
+    // counting the bits gets you the equivalent values XX (2), Xx (1), or xx (0), while ignoring gene order
+    const dilute = countSetBits(genotype & 3);
+    const tabby = countSetBits((genotype >> 2) & 3);
+    const red = countSetBits((genotype >> 4) & 3);
+    const white = countSetBits((genotype >> 6) & 3);
+    const point = countSetBits((genotype >> 8) & 3);
+    const muzzleLength = countSetBits((genotype >> 10) & 0xF);
+    const eyeShape = countSetBits((genotype >> 14) & 0xF);
+    const headSize = countSetBits((genotype >> 18) & 0xF);
+    const earSet = countSetBits((genotype >> 22) & 0xF);
+
+    const phenotype = {
+        headSize,
+        earSet,
+        eyeShape,
+        muzzleLength,
+        tabby: (tabby > 0) ? 'tabby' : 'non-tabby',
+        dilute: (dilute === 2) ? 'dilute' : 'non-dilute',
+        point: (point === 2) ? 'point' : 'non-point',
+        red: (red === 2) ? 'red' : ((red === 1) ? 'tortie' : 'non-red'),
+        white: (white === 2) ? 'high' : ((white === 1) ? 'low' : 'non-white')
+    };
+
+    return phenotype;
+}
+
+export function phenotypeToColors(phenotype) {
+    let base = (phenotype.red === 'red') ? 'red' : 'black';
+
+    if(phenotype.dilute === 'dilute') base += 'dilute';
+    if(phenotype.tabby === 'tabby') base += 'tabby';
+    if(phenotype.point === 'point') base += 'point';
+
+    const redBase = base.replace('black', 'red'); // essentially ignored on already red cats
+
+    const colors = {
+        head: (phenotype.point === 'point') ? palette['point'] : palette[base],
+        leftEar: palette[base],
+        rightEar: (phenotype.red === 'tortie') ? palette[redBase] : palette[base],
+        eyes: (phenotype.white === 'high' || phenotype.point === 'point') ? palette['blue'] : palette['yellow'],
+        muzzle: (phenotype.white !== 'non-white') ? palette['white'] : palette[base],
+        jaw: (phenotype.white !== 'non-white') ? palette['white'] : ((phenotype.red === 'tortie') ? palette[redBase] : palette[base]),
+        stripes: palette[base.replace('tabby', '')],
+        tortie: palette[redBase],
+        tortieStripes: palette[redBase.replace('tabby', '')],
+        point: palette[base],
+        white: palette['white']
+    };
+
+    return colors;
+}
+
+export function comparePhenotypes(phenotype1, phenotype2) {
+    for(const gene in phenotype1) {
+        if(phenotype1[gene] !== phenotype2[gene]) return false;
+    }
+    return true;
+}
