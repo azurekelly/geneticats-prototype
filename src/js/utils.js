b3c1ebe47dd4ed9043c276b30dffd52e96b693cb
Azure Kelly <azkbaby@gmail.com>
2020-06-07 06:27:33 -0700

    Remove missed leftovers of Snap/jQuery

--- src/js/utils.js
+++ src/js/utils.js
@@ -9,10 +9,6 @@ export function countSetBits(n) {
     return count;
 }
 
-export function fill(el, fill) {
-    el.attr({'fill': fill});
-}
-
 export function randomBetween(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
