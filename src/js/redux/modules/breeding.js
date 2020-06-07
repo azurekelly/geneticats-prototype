49dcfcdd474bf12782012bda65006cf92e55e11e
Azure Kelly <azkbaby@gmail.com>
2020-06-07 04:02:48 -0700

    Add breeding state module

---
+++ src/js/redux/modules/breeding.js
@@ -0,0 +1,69 @@
+import {combineReducers} from 'redux';
+
+// ACTIONS
+const START_SELECTING = 'START_SELECTING_PARENT';
+const SELECT = 'SELECT_PARENT';
+const CANCEL = 'CANCEL_BREEDING';
+
+// ACTION CREATORS
+export const startSelectingParent = targetSlot => ({
+    type: START_SELECTING,
+    payload: targetSlot
+});
+export const selectParent = (target, cat) => ({
+    type: SELECT,
+    payload: {
+        target,
+        cat
+    }
+});
+export const cancelParentSelection = () => ({
+    type: CANCEL
+});
+
+// SELECTORS
+export const isSelectingSelector = state => state.breeding.isSelecting;
+export const targetParentSlotSelector = state => state.breeding.targetSlot;
+export const breedingParentsSelector = state => state.breeding.parents;
+
+//REDUCERS
+const isSelectingReducer = (state = false, action) => {
+    switch(action.type) {
+        case START_SELECTING:
+            return true;
+        case SELECT:
+        case CANCEL:
+            return false;
+        default:
+            return state;
+    }
+};
+const targetSlotReducer = (state = null, action) => {
+    switch(action.type) {
+        case START_SELECTING:
+            return action.payload;
+        case SELECT:
+        case CANCEL:
+            return null;
+        default:
+            return state;
+    }
+};
+const parentsReducer = (state = [], action) => {
+    switch(action.type) {
+        case SELECT:
+            return state.map((cat, i) => (i == action.payload.target ? action.payload.cat : cat));
+        case CANCEL:
+            return [];
+        default:
+            return state;
+    }
+};
+
+const allReducers = combineReducers({
+    isSelecting: isSelectingReducer,
+    targetSlot: targetSlotReducer,
+    parents: parentsReducer
+});
+
+export default allReducers;
--- src/js/redux/rootReducer.js
+++ src/js/redux/rootReducer.js
@@ -2,12 +2,14 @@ import {combineReducers} from 'redux';
 import route from './modules/route';
 import catStore from './modules/catStore';
 import {scoreReducer, goalReducer} from './modules/goal';
+import breeding from './modules/breeding';
 
 const rootReducer = combineReducers({
     route,
     catStore,
     score: scoreReducer,
-    goal: goalReducer
+    goal: goalReducer,
+    breeding
 });
 
 export default rootReducer;
